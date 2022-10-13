522dd7fdcc6fd1d8d67595efe17b58dbc7fb268a
Konstantin <kain7984@gmail.com>
2022-10-13 19:15:30 +0800

    Added method personList in Main

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,18 @@ import java.util.Collections;
 
 class Main {
 
-    public static void main(String[] args) {
+    private static List<Person> personList() {
         List<Person> person = new ArrayList<>();
         person.add(new Person("Alex", "Petrov-Ivanov", 20));
         person.add(new Person("Katrine", "Gonzalez Fernandez, Jose", 18));
-        person.add(new Person("Max", "Ivanov", 30));
-        person.add(new Person("Mary", "Johnson-Marcy", 22));
+        person.add(new Person("Max", "Alexandros", 30));
+        person.add(new Person("Mary", "Johnson-Frederik", 22));
 
+        return person;
+    }
+
+    public static void main(String[] args) {
+        List<Person> person = personList();
         Collections.sort(person, new SurnameAgeComparator(1));
         System.out.println(person);
     }
